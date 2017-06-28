3520f3b3a7c00c1e6758db27c96f0187a7dcb041
Alex <Alex@ALEX-HP>
2017-06-28 12:23:49 -0700

    Add project files.

---
+++ TimeWorkedCalculator/MainForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace TimeWorkedCalculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.txtLogs = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAvgTime = new System.Windows.Forms.TextBox();
+            this.txtTasksSubmitted = new System.Windows.Forms.TextBox();
+            this.txtBinary = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Time Worked:";
+            // 
+            // txtHours
+            // 
+            this.txtHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtHours.Location = new System.Drawing.Point(123, 125);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.ReadOnly = true;
+            this.txtHours.Size = new System.Drawing.Size(74, 20);
+            this.txtHours.TabIndex = 1;
+            // 
+            // txtLogs
+            // 
+            this.txtLogs.Location = new System.Drawing.Point(456, 12);
+            this.txtLogs.Multiline = true;
+            this.txtLogs.Name = "txtLogs";
+            this.txtLogs.ReadOnly = true;
+            this.txtLogs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLogs.Size = new System.Drawing.Size(192, 284);
+            this.txtLogs.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::TimeWorkedCalculator.Properties.Resources.zerochaos_logo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(75, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 67);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Avg. Time/Task:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tasks Submitted:";
+            // 
+            // txtAvgTime
+            // 
+            this.txtAvgTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtAvgTime.Location = new System.Drawing.Point(123, 151);
+            this.txtAvgTime.Name = "txtAvgTime";
+            this.txtAvgTime.ReadOnly = true;
+            this.txtAvgTime.Size = new System.Drawing.Size(74, 20);
+            this.txtAvgTime.TabIndex = 8;
+            this.txtAvgTime.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtTasksSubmitted
+            // 
+            this.txtTasksSubmitted.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtTasksSubmitted.Location = new System.Drawing.Point(123, 178);
+            this.txtTasksSubmitted.Name = "txtTasksSubmitted";
+            this.txtTasksSubmitted.ReadOnly = true;
+            this.txtTasksSubmitted.Size = new System.Drawing.Size(74, 20);
+            this.txtTasksSubmitted.TabIndex = 9;
+            // 
+            // txtBinary
+            // 
+            this.txtBinary.Location = new System.Drawing.Point(5, 302);
+            this.txtBinary.Multiline = true;
+            this.txtBinary.Name = "txtBinary";
+            this.txtBinary.ReadOnly = true;
+            this.txtBinary.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBinary.Size = new System.Drawing.Size(408, 196);
+            this.txtBinary.TabIndex = 10;
+            this.txtBinary.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(654, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(203, 12);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInput.Size = new System.Drawing.Size(247, 284);
+            this.txtInput.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(72, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Compute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TimeWorkedCalculator.Properties.Resources.background_light;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(654, 298);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtBinary);
+            this.Controls.Add(this.txtTasksSubmitted);
+            this.Controls.Add(this.txtAvgTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtLogs);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Time Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.TextBox txtLogs;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAvgTime;
+        private System.Windows.Forms.TextBox txtTasksSubmitted;
+        private System.Windows.Forms.TextBox txtBinary;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TimeWorkedCalculator/MainForm.cs
@@ -0,0 +1,136 @@
+﻿/*
+ * Program Name: Time Calculator 
+ * Programmer: Oleksii Butakov
+ * Purpose: This program was made by me in the purpose to make my life easier and to automate the process of calculating time I spent working. 
+ * How it's works: To calculate the time, you need to copy history logs from the employer's website (see example in the project's root folder) and paste it into the left textbox. 
+ *                 Then press "Compute", and the program will return detailed information, such as: average time per task spent, amount of tasks submitted, total time spent. 
+ * Last Modified: 6/28/2017
+ */
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TimeWorkedCalculator
+{
+    public partial class MainForm : Form
+    {
+        String line;
+        String prevLine;
+
+        DateTime time;
+        DateTime prevTime;
+        
+        TimeSpan span;
+
+        double totalMinutes;
+
+        int numIterations = 0;
+
+        const short JUNK_CHARACTERS = 20;
+        const short MAX_TIME_PER_TASK = 14;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtBinary.BackColor = Color.Black;
+            txtBinary.ForeColor = Color.ForestGreen;
+            
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            bool isFirstIteration = false;
+            short counter = 0;
+            totalMinutes = 0;
+
+            if (txtInput.Lines.Length != 0)
+            {
+                for (int i = 0; i < txtInput.Lines.Length; i++)
+                {
+                    line = txtInput.Lines[i];
+                    if (prevLine == null)
+                    {
+                        prevLine = line.Remove(line.Length - JUNK_CHARACTERS);
+                        prevTime = DateTime.Parse(prevLine);
+                        isFirstIteration = true;
+                    }
+                    else
+                    {
+                        if (line.Length > JUNK_CHARACTERS)
+                        {
+                            line = line.Remove(line.Length - JUNK_CHARACTERS);
+
+                            time = DateTime.Parse(line);
+
+                            span = prevTime.Subtract(time);
+
+                            txtLogs.AppendText("#" + (++counter).ToString() + Environment.NewLine);
+                            txtLogs.AppendText(prevTime.ToShortTimeString() + Environment.NewLine);
+                            txtLogs.AppendText(time.ToShortTimeString() + Environment.NewLine);
+
+                            prevTime = time;
+                            isFirstIteration = false;
+                        }
+                        else
+                            continue;
+                    }
+
+
+                    numIterations++;
+
+                    if (!isFirstIteration)
+                    {
+                        if (span.Hours == 0 && span.Minutes < MAX_TIME_PER_TASK)
+                        {
+                            if (span.Minutes != 0)
+                                totalMinutes += span.Minutes;
+                            else
+                                totalMinutes++;
+
+                            txtLogs.AppendText("Time Spent: " + span.Hours + "h " + span.Minutes + "m " + "---- ADDED");
+                            txtLogs.AppendText(Environment.NewLine + Environment.NewLine);
+                        }
+                        else
+                        {
+                            txtLogs.AppendText("Time Spent: " + span.Hours + "h " + span.Minutes + "m" + "---- DECLINED");
+                            txtLogs.AppendText(Environment.NewLine + Environment.NewLine);
+                        }
+                    }
+                }
+                // casting total minutes (int) into (TimeSpan data type 
+                span = TimeSpan.FromMinutes(totalMinutes);
+
+                // Writing down logs 
+                txtLogs.AppendText("==========================");
+                txtLogs.AppendText(Environment.NewLine);
+                txtLogs.AppendText("Total Time: " + span.ToString(@"hh\:mm") + " hrs");
+                txtLogs.AppendText(Environment.NewLine);
+                txtLogs.AppendText("Tasks submitted: " + counter.ToString());
+                txtLogs.AppendText(Environment.NewLine);
+                txtLogs.AppendText("Task/Time Ratio: " + (TimeSpan.FromMinutes(span.TotalMinutes / counter)).ToString(@"mm\.ss") + " m.");
+                txtLogs.AppendText(Environment.NewLine);
+                txtLogs.AppendText("Iterations: " + numIterations.ToString());
+                txtLogs.AppendText(Environment.NewLine);
+
+                // Displaying output
+                txtTasksSubmitted.Text = counter.ToString();
+                txtAvgTime.Text = (TimeSpan.FromMinutes(span.TotalMinutes / counter)).ToString(@"mm\.ss") + " m";
+                txtHours.Text = span.ToString(@"hh\:mm") + " hrs";
+            }
+            else
+                txtLogs.AppendText("ERROR: Input Textbox is empty");
+        }
+    }
+}
+
